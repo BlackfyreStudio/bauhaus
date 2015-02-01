0191734ba4ede9db668fea14efa86a8535361e95
blackfyre <gnick666@gmail.com>
2015-02-01 07:42:18 +0100

    added raw language files for Hungarian translation

---
+++ src/lang/hu/form.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'title' => [
+		'edit-model'   => 'Edit :model',
+		'create-model' => 'Create new :model'
+	],
+
+	'button' => [
+		'back-to-index' => 'Back to :model',
+		'save'          => 'Save :model'
+	],
+
+	'modal' => [
+		'loading' => 'Loading...'
+	]
+
+];
---
+++ src/lang/hu/messages.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the KraftHaus Bauhaus package.
+ *
+ * (c) KraftHaus <hello@krafthaus.nl>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+return [
+
+	'success' => [
+		'title'         => 'Success!',
+		'model-created' => 'Created a new :model',
+		'model-updated' => 'Updated a :model',
+		'model-deleted' => 'Deleted :count :model'
+	],
+
+	'warning' => [
+		'title' => 'Warning!'
+	],
+
+	'error' => [
+		'title'             => 'Whoops!',
+		'validation-errors' => 'Validation errors'
+	]
+
+];
